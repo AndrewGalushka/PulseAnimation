c1821c9b75174b762fb6122a734dfb067abc7098
AndrewGalushka <andrewgalushka@yahoo.com>
2018-04-15 14:34:18 +0400

    rf

--- Pulse/TouchDownCircleAnimationViewController.swift
+++ Pulse/TouchDownCircleAnimationViewController.swift
@@ -12,24 +12,37 @@ class TouchDownCircleAnimationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        addTouchDownCircleDrawingGesture()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func addTouchDownCircleDrawingGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(touchDownCircleDrawingGestureHandler)))
     }
-    */
-
+    
+    func touchDownCircleDrawingGestureHandler(tapGesure tap: UITapGestureRecognizer) {
+        
+        let gestureLocation = tap.location(in: view)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame = CGRect(x: gestureLocation.x, y: gestureLocation.y, width: 200, height: 200)
+        shapeLayer.position = gestureLocation
+        shapeLayer.strokeColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 4.0
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.random().cgColor
+        shapeLayer.strokeEnd = 0.0
+        shapeLayer.path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 200, height: 200)).cgPath
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        let circleDrawAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        circleDrawAnimation.toValue = 1.0
+        circleDrawAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        circleDrawAnimation.duration = 2.0
+        circleDrawAnimation.autoreverses = true
+        circleDrawAnimation.repeatCount = 2
+        
+        shapeLayer.add(circleDrawAnimation, forKey: nil)
+    }
 }
